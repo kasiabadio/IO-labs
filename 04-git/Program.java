1e1682c176756b3ba9b145dcaaddef688015c7c5
Kasia <kasiabadio@hotmail.com>
2021-11-01 10:15:17 +0100

    Pierwsza zmiana
    Druga zmiana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,7 +2,13 @@
 // Print Hello <name>
 public class Program {
 
+    public void print_name(String _name){
+        System.out.println("Hello, " + _name);
+    }
+
     public static void main(String[] args) {
+
+        Program program = new Program();
         // Prints "Hello, World" to the terminal window.
         program.print_name("Kasia");
     }
